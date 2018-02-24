62e7e148cf7ac5e0ddfad43f260c95a86a78ae46
sameerb3737 <36567068+sameerb3737@users.noreply.github.com>
2018-02-24 16:11:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -283,43 +283,7 @@ def getChaperObject(chapterConxtext):
 
     return myobjectx
 
-def getContextParameters(contexts):
-    parameters = [1,2,3,4]
-    if len(contexts) == 3:
-	if 'chapter' in contexts[0].get('name'):
-            if 'q' in contexts[1].get('name'):
-                testpaper = contexts[2].get('name')
-                questionnumber = contexts[1].get('name')
-                chapterContext = contexts[0].get('name')
-            else:
-                testpaper = contexts[1].get('name')
-                questionnumber = contexts[2].get('name')
-                chapterContext = contexts[0].get('name')
-        else:
-    
-            if 'chapter' in contexts[1].get('name'):
-                if 'q' in contexts[0].get('name'):
-                    testpaper = contexts[2].get('name')
-                    questionnumber = contexts[0].get('name')
-                    chapterContext = contexts[1].get('name')
-                else:
-                    testpaper = contexts[0].get('name')
-                    questionnumber = contexts[2].get('name')
-                    chapterContext = contexts[1].get('name')
-            if 'chapter' in contexts[2].get('name'):
-                if 'q' in contexts[1].get('name'):
-                    testpaper = contexts[0].get('name')
-                    questionnumber = contexts[1].get('name')
-                    chapterContext = contexts[2].get('name')
-                else:
-                    testpaper = contexts[1].get('name')
-                    questionnumber = contexts[0].get('name')
-                    chapterContext = contexts[2].get('name')
 
-    if len(contexts) == 4:
-	
-	
-    return parameters
 	
 def random_line(fname):
     lines = open(fname).read().splitlines()
